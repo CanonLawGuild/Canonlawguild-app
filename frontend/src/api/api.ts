b7724cb744da8a5c96c19e3106a3ff4f7cc3052b
kayanja2024 <paulkayanja1989@gmail.com>
2026-09-11 15:50:18 +0300

    testing the live url for backend again

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 // const API_BASE = import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000'
-const API_BASE = import.meta.env.VITE_API_BASE || 'https://canonlawguild-backend.onrender.com'
+// const API_BASE = import.meta.env.VITE_API_BASE || 'https://canonlawguild-backend.onrender.com'
+const API_BASE = import.meta.env.VITE_API_BASE || 'https://onrender.com'
+
 
 const api = axios.create({
   baseURL: API_BASE,
